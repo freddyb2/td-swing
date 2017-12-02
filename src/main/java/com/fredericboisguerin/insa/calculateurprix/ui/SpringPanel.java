b9393f9bbb7b7499bae2fd9f57a305cb9913b7ea
Fred <frederic.boisguerin@gmail.com>
2017-12-02 16:15:57 +0100

    Refactor SpringPanel

--- src/main/java/com/fredericboisguerin/insa/calculateurprix/ui/SpringPanel.java
+++ src/main/java/com/fredericboisguerin/insa/calculateurprix/ui/SpringPanel.java
@@ -9,29 +9,19 @@ public class SpringPanel extends JPanel {
     }
 
     public void makeCompactGrid(int numberOfRows, int numberOfColumns, int margin, int padding) {
-        //Align all cells in each column and make them the same width.
-        Spring x = Spring.constant(margin);
-        for (int columnId = 0; columnId < numberOfColumns; columnId++) {
-            Spring width = Spring.constant(0);
-            for (int r = 0; r < numberOfRows; r++) {
-                width = Spring.max(width, getConstraintsForCell(r, columnId, numberOfColumns).
-                        getWidth());
-            }
-            for (int r = 0; r < numberOfRows; r++) {
-                SpringLayout.Constraints constraints = getConstraintsForCell(r, columnId, numberOfColumns);
-                constraints.setX(x);
-                constraints.setWidth(width);
-            }
-            x = Spring.sum(x, Spring.sum(width, Spring.constant(padding)));
-        }
+        Spring x = alignCellsVertically(numberOfRows, numberOfColumns, margin, padding);
+        Spring y = alignCellsHorizontally(numberOfRows, numberOfColumns, margin, padding);
+        SpringLayout.Constraints constraints = getLayout().getConstraints(this);
+        constraints.setConstraint(SpringLayout.SOUTH, y);
+        constraints.setConstraint(SpringLayout.EAST, x);
+    }
 
-        //Align all cells in each row and make them the same height.
+    private Spring alignCellsHorizontally(int numberOfRows, int numberOfColumns, int margin, int padding) {
         Spring y = Spring.constant(margin);
         for (int r = 0; r < numberOfRows; r++) {
             Spring height = Spring.constant(0);
             for (int c = 0; c < numberOfColumns; c++) {
-                height = Spring.max(height, getConstraintsForCell(r, c, numberOfColumns).
-                        getHeight());
+                height = Spring.max(height, getConstraintsForCell(r, c, numberOfColumns).getHeight());
             }
             for (int c = 0; c < numberOfColumns; c++) {
                 SpringLayout.Constraints constraints = getConstraintsForCell(r, c, numberOfColumns);
@@ -40,11 +30,24 @@ public class SpringPanel extends JPanel {
             }
             y = Spring.sum(y, Spring.sum(height, Spring.constant(padding)));
         }
+        return y;
+    }
 
-        // Set this panel's size
-        SpringLayout.Constraints pCons = getLayout().getConstraints(this);
-        pCons.setConstraint(SpringLayout.SOUTH, y);
-        pCons.setConstraint(SpringLayout.EAST, x);
+    private Spring alignCellsVertically(int numberOfRows, int numberOfColumns, int margin, int padding) {
+        Spring x = Spring.constant(margin);
+        for (int columnId = 0; columnId < numberOfColumns; columnId++) {
+            Spring width = Spring.constant(0);
+            for (int r = 0; r < numberOfRows; r++) {
+                width = Spring.max(width, getConstraintsForCell(r, columnId, numberOfColumns).getWidth());
+            }
+            for (int r = 0; r < numberOfRows; r++) {
+                SpringLayout.Constraints constraints = getConstraintsForCell(r, columnId, numberOfColumns);
+                constraints.setX(x);
+                constraints.setWidth(width);
+            }
+            x = Spring.sum(x, Spring.sum(width, Spring.constant(padding)));
+        }
+        return x;
     }
 
     @Override
